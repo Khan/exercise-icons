b67a7b4837de2cfd2036594eb9fb1494904879a4
Jared Forsyth <jared@khanacademy.org>
2014-06-12 17:14:30 -0700

    wip

--- capture.js
+++ capture.js
@@ -18,14 +18,11 @@
  */
 
 var casper = require("casper").create();
+var addProximaNova = require('./add-proxima-nova.js');
 
 casper.on('remote.message', function(message) {
     console.log(message);
 });
-
-var itemId;
-var mode;
-var seed;
 
 if (casper.cli.args.length < 1) {
     casper
@@ -41,71 +38,50 @@ if (casper.cli.args.length < 1) {
         .echo("  - Get more help:")
         .echo("         $ cat ./capture.js")
         .exit(1);
+}
+
+var itemId;
+var mode;
+var seed;
+
+itemId = casper.cli.args[0];
+if (!itemId.length) {
+    casper
+        .echo("Item id is empty")
+        .exit(1);
+}
+
+// Introduce some pseudo-randomness because 1-digit division
+// and basic division have the same screenshot for seed=43.
+seed = 42;
+for (var i = 0; i < itemId.length; ++i) {
+    seed += itemId.charCodeAt(i);
+    seed %= 200;
+};
+
+// No khan-exercise starts with 'x'. If that changes, this will also have
+// to change.
+if (itemId[0] === 'x') {
+    mode = "perseus";
+    screenshotUrl = "https://www.khanacademy.org/preview/content/items/" + itemId;
 } else {
-    itemId = casper.cli.args[0];
-    if (!itemId.length) {
-        casper
-            .echo("Item id is empty")
-            .exit(1);
-    }
-
-    // Introduce some pseudo-randomness because 1-digit division
-    // and basic division have the same screenshot for seed=43.
-    seed = 42;
-    for (var i = 0; i < itemId.length; ++i) {
-        seed += itemId.charCodeAt(i);
-        seed %= 200;
-    };
-
-    // No khan-exercise starts with 'x'. If that changes, this will also have
-    // to change.
-    if (itemId[0] === 'x') {
-        mode = "perseus";
-        screenshotUrl = "https://www.khanacademy.org/preview/content/items/" + itemId;
-    } else {
-        mode = "ke";
-        screenshotUrl = "http://sandcastle.kasandbox.org/media/castles/Khan:master/exercises/" +
-            itemId + ".html?debug&seed=" + (++seed);
-    }
+    mode = "ke";
+    screenshotUrl = "http://sandcastle.kasandbox.org/media/castles/Khan:master/exercises/" +
+        itemId + ".html?debug&seed=" + (++seed);
 }
 
 casper.start(screenshotUrl, function() {
     this.echo('Current location is ' + this.getCurrentUrl(), 'info');
     if (mode === "ke") {
         // Fix for Proxima Nova not being loaded on sandcastle.
-        this.evaluate(function() {
-            var newStyle = document.createElement('style');
-            newStyle.appendChild(document.createTextNode(
-                "@font-face {" + 
-                "    font-family: 'Proxima Nova';" + 
-                "    src: url('https://www.kastatic.org/fonts/ProximaNova-Reg-webfont.woff') format('woff');" + 
-                "    font-weight: normal;" + 
-                "    font-style: normal;" + 
-                "}" + 
-                "" + 
-                "@font-face {" + 
-                "    font-family: 'Proxima Nova';" + 
-                "    src: url('https://www.kastatic.org/fonts/ProximaNova-Bold-webfont.woff') format('woff');" + 
-                "    font-weight: bold;" + 
-                "    font-style: normal;" + 
-                "}" + 
-                "" + 
-                "@font-face {" + 
-                "    font-family: 'Proxima Nova Bold';" + 
-                "    src: url('https://www.kastatic.org/fonts/ProximaNova-Bold-webfont.woff') format('woff');" + 
-                "    font-weight: normal;" + 
-                "    font-style: normal;" + 
-                "}" +
-                "body {" +
-                "   font-family: 'Proxima Nova';" +
-                "}"));
-            document.head.appendChild(newStyle);
-        });
+        this.evaluate(addProximaNova);
     }
 });
 
 var bb;
 var currIdx = -1;
+
+var boundingBox = require('./bounding-box.js');
 
 function takeSnapshot(casper) {
     this.then(function() {
@@ -114,42 +90,6 @@ function takeSnapshot(casper) {
     });
     this.then(function() {
         bb = this.evaluate(function(width, height, mode) {
-            var boundingBox = function(items) {
-                var maxX, maxY, minX, minY;
-                function reset() {
-                    maxX = 0;
-                    maxY = 0;
-                    minX = 10000;
-                    minY = 10000;
-                }
-                reset();
-                _.each(items, function(item) {
-                    var bb = {
-                        x: $(item).offset().left,
-                        y: $(item).offset().top,
-                        width: $(item).width(),
-                        height: $(item).height()
-                    };
-                    if (bb.width > 100 && bb.height > 100 &&
-                            bb.height/bb.width > 0.75 &&
-                            bb.height/bb.width < 1.5 ) {
-                        reset();
-                    } else if (maxX && maxX + bb.x + bb.width  - maxX > 50) {
-                        return;
-                    }
-                    minX = Math.min(bb.x, minX);
-                    minY = Math.min(bb.y, minY);
-                    maxX = Math.max(bb.x + bb.width, maxX);
-                    maxY = Math.max(bb.y + bb.height, maxY);
-                });
-                var ret = {
-                    left: minX,
-                    top: minY,
-                    width: maxX - minX,
-                    height: maxY - minY
-                };
-                return ret;
-            }
 
             // We need '?debug=1' to see the problem types, but we don't
             // want the debug borders.
