5652971c84734adc6ae7200b5cf1e8a488b8d582
Emily Eisenberg <xymostech@gmail.com>
2015-08-21 10:44:41 -0700

    And some more stuff!

--- slimer-icons.js
+++ slimer-icons.js
@@ -1,5 +1,35 @@
-var getInterestingBox = require('./lib/capture/client/get-interesting-box.js');
-var addProximaNova = require('./lib/capture/client/add-proxima-nova.js');
+/*
+ * To use:
+ *
+ * Download an updated exercises.json, by running in BigQuery:
+ *
+ *  SELECT (CASE WHEN file_name = '' or file_name is null THEN 'perseus' ELSE 'khan-exercises' END) as type,
+ *  name, file_name, NTH(1, problem_types.items.id) as example_item
+ *  FROM [latest_content.Exercise]
+ *  GROUP BY type, name, file_name
+ *
+ * Download the result as a CSV, and convert to JSON by running in python (for
+ * some reason the 'download as JSON' doesn't produce valid JSON)
+ *
+ *  import csv
+ *  import json
+ *  json.dump([x for x in csv.DictReader(open("/path/to/results.csv"))], open('exercises.json', 'w'))
+ *
+ * Now, take all the screenshots by running this script:
+ *
+ *  mkdir slimer-raw
+ *  slimerjs slimer-icons.js
+ *
+ * Finally, crop the images with imagemagick via:
+ *
+ *  mkdir slimer-thumbs
+ *  cd slimer-raw
+ *  for file in *; do echo ${file%.png}; convert -trim -- $file ../slimer-thumbs/${file%png}.jpg; done
+ *
+ * Yay!
+ */
+
+var OUTDIR = "slimer-raw";
 
 function waitForSelector(page, selector) {
     while (true) {
@@ -35,12 +65,88 @@ function waitForImages(page) {
     }
 }
 
+function addProximaNova() {
+    var newStyle = document.createElement('style');
+    newStyle.appendChild(document.createTextNode(
+        "@font-face {" +
+        "    font-family: 'Proxima Nova';" +
+        "    src: url('https://www.kastatic.org/fonts/Proxima-Nova-Regular.woff') format('woff');" +
+        "    font-weight: normal;" +
+        "    font-style: normal;" +
+        "}" +
+        "" +
+        "@font-face {" +
+        "    font-family: 'Proxima Nova';" +
+        "    src: url('https://www.kastatic.org/fonts/Proxima-Nova-Semibold.woff') format('woff');" +
+        "    font-weight: bold;" +
+        "    font-style: normal;" +
+        "}" +
+        "" +
+        "@font-face {" +
+        "    font-family: 'Proxima Nova Bold';" +
+        "    src: url('https://www.kastatic.org/fonts/Proxima-Nova-Semibold.woff') format('woff');" +
+        "    font-weight: normal;" +
+        "    font-style: normal;" +
+        "}" +
+        "body {" +
+        "   font-family: 'Proxima Nova';" +
+        "}"));
+    document.head.appendChild(newStyle);
+}
+
+function getInterestingBox() {
+
+    // this must be embedded, because the outer function is being injected
+    // into the browser's javascript runtime.
+    var boundingBox = function(items) {
+        var box = {
+            minX: 1.0 / 0,
+            minY: 1.0 / 0,
+            maxX: 0,
+            maxY: 0
+        };
+
+        _.each(items, function(item) {
+            var bb = {
+                x: $(item).offset().left,
+                y: $(item).offset().top,
+                width: $(item).width(),
+                height: $(item).height()
+            };
+
+            box = {
+                minX: Math.min(bb.x, box.minX),
+                minY: Math.min(bb.y, box.minY),
+                maxX: Math.max(bb.x + bb.width, box.maxX),
+                maxY: Math.max(bb.y + bb.height, box.maxY)
+            };
+        });
+
+        return {
+            left: box.minX - 3,
+            top: box.minY - 3,
+            width: box.maxX - box.minX + 6,
+            height: box.maxY - box.minY + 6
+        };
+    };
+
+    var interesting_things = [];
+
+    var $pretties = $("#workarea");
+    interesting_things.forEach(function(name) {
+        $pretties = $pretties.add('#workarea ' + name);
+    });
+    return boundingBox($pretties);
+}
+
+
 function shootKhan(page, name, dest) {
     page.evaluate(addProximaNova);
     waitForSelector(page, "#workarea");
     waitForImages(page);
     slimer.wait(1000);
     page.evaluate(function() {
+        $("body").removeClass("debug");
         $("div.qtip").hide();
         $("input").blur();
     });
@@ -105,9 +211,9 @@ function shootExercises(page, currIndex) {
         page.open(url)
             .then(function() {
                 if (exercise.type === "khan-exercises") {
-                    shootKhan(page, exercise.name, 'slimerraw');
+                    shootKhan(page, exercise.name, OUTDIR);
                 } else {
-                    shootPerseus(page, exercise.name, 'slimerraw');
+                    shootPerseus(page, exercise.name, OUTDIR);
                 }
                 doLog(exercise.type, exercise.name);
                 shootExercises(page, currIndex + numPages);
