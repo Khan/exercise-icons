0bc6b77e4319df8fa78e5ef5dffa079e3b16376f
Craig Silverstein <csilvers@khanacademy.org>
2016-02-29 11:34:26 -0800

    Print a banner every time we start a new run, to make it easier to tell (in the logs).
    Auditors: chris
    
    Test plan:
    Ran the commands manually.

--- full-run.sh
+++ full-run.sh
@@ -16,5 +16,10 @@ export S3_KEY S3_SECRET S3_BUCKET
 # We need to make sure we pick up the right versions of casperjs and phantomjs.
 export PATH="$PWD/node_modules/casperjs/bin:$PWD/node_modules/phantomjs/bin:$PATH"
 
+echo
+echo ---------------------------------------------------------------------
+echo Starting capture.js run at `date`
+echo ---------------------------------------------------------------------
+
 # Prepend  DEBUG=* on the line below for more verbosity
 ./bin/capture.js --upload --all --parallel 1
